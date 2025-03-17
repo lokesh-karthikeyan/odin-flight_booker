911d11433c57b8df6f042de37ed815fe65d3e195
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-03-17 17:26:27 +0530

    Model: Add "Active Record Associations"

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,4 @@
 class Flight < ApplicationRecord
-  belongs_to :departure_airport
-  belongs_to :arrival_airport
+  belongs_to :departure_airport, class_name: "Airport", foreign_key: "airport_id"
+  belongs_to :arrival_airport, class_name: "Airport", foreign_key: "airport_id"
 end
