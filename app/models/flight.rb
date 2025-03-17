c84a9645997d9adee8e2876d63f05161dd282fca
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-03-17 17:50:28 +0530

    Model: Fix "foreign_key" error

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,4 @@
 class Flight < ApplicationRecord
-  belongs_to :departure_airport, class_name: "Airport", foreign_key: "airport_id"
-  belongs_to :arrival_airport, class_name: "Airport", foreign_key: "airport_id"
+  belongs_to :departure_airport, class_name: "Airport"
+  belongs_to :arrival_airport, class_name: "Airport"
 end
