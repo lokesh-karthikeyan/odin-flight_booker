ce86e69c0ed415e3e846ca072e88cf71f286ded3
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-03-18 20:27:49 +0530

    Controller: Add `#new, #create, #show` action controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,26 @@
 class BookingsController < ApplicationController
+  def new
+    @flight = Flight.find(params[:flight])
+    @passenger_count = params[:passengers].to_i
+    @booking = Booking.new
+    @passenger_count.times { @booking.passengers.build }
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    Rails.logger.debug "ZZZZZ #{params}"
+    if @booking.save
+      redirect_to @booking, notice: "Booking was successful!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show = (@booking = Booking.find(params[:id]))
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [ :name, :email ])
+  end
 end
