a5c7ec6dbb06dd52637156ce63f0ac9809b9e0d0
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-03-18 15:46:56 +0530

    Controller: Add `#index` action controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,13 @@
 class FlightsController < ApplicationController
+  def index
+    @locations = Airport.all.order(city: :asc).pluck(:city, :code)
+    @passengers = [ *1..4 ]
+    @dates = Flight.all.order(starts_at: :asc).map { |flight| flight.starts_at.strftime("%d-%m-%Y | %I:%M %p") }.uniq
+    @available_flights = nil || Flight.find_flights(flight_params)
+    @passenger_count = params[:passengers]
+  end
+
+  private
+
+  def flight_params = params.permit(:departure, :destination, :date_and_time, :passengers)
 end
